b2ac00cf0985befd046cd3df0526f2fa1aad6c17
Nee_nnick <Yannickgarbe02@gmail.com>
2025-07-27 16:39:32 +0100

    gun + muzzleFlash

---
+++ Assets/Script/PlayerScripts/GunScript.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public class GunScript : MonoBehaviour
+{
+    [SerializeField] int damage = 10;
+    [SerializeField] float range = 100f;
+    [SerializeField] float fireRate = 10f;
+    [SerializeField] Camera playerCam;
+    [SerializeField] ParticleSystem muzzleFlash;
+    [SerializeField] GameObject impactEffect;
+
+    [SerializeField] float fireInterval = .5f;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        //fire bullet
+        if (Input.GetMouseButtonDown(0) && Time.time >= fireInterval)
+        {
+            fireInterval = Time.time + 1f / fireRate;
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        if(muzzleFlash != null)
+        {
+            muzzleFlash.Play();
+        }
+
+        RaycastHit hit;
+        //send raycast
+        if(Physics.Raycast(playerCam.transform.position, playerCam.transform.forward, out hit, range))
+        {
+            //find target health
+            Health targetHealth = hit.transform.GetComponent<Health>();
+
+            if(targetHealth != null)
+            {
+                targetHealth.Sethealth(damage);
+            }
+
+            //add impactEffect
+            if(impactEffect != null)
+            {
+                GameObject impactGo = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+                Destroy(impactGo, 2f);
+            }
+        }
+    }
+}
